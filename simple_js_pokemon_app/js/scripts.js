fc08043767208c966b2551461769625cda32bec2
Peppermint-Mocha <jackimuthumbiyomade@gmail.com>
2021-10-27 21:16:28 +0200

    Adding pokemon files to be monitered by Git

---
+++ simple_js_pokemon_app/js/scripts.js
@@ -0,0 +1,14 @@
+//alert('Hello PalmTree')
+
+/*let myName= "Dodo";
+document.write(myName);
+
+let myFavouriteFood="Falafel";
+document.write(myFavouriteFood);*/
+
+let pokemonList = [
+        firstPokemon = { name : 'bulbasaur', height : 7, types : ['grass ', 'poison']},
+        secondPokemon = {name : 'squirtl', height: 5, types : "water"},
+        thirdPokemon = {name :' wartortle', height: 10, type : "water" },
+
+];
