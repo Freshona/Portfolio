85f53a08b12b7befcdf87ef6c511cabd6283807f
Peppermint-Mocha <jackimuthumbiyomade@gmail.com>
2021-10-31 14:55:22 +0100

    added a for loop and used documentwrite to list objects of pokemonlist to the dom

--- simple_js_pokemon_app/js/scripts.js
+++ simple_js_pokemon_app/js/scripts.js
@@ -9,6 +9,14 @@ document.write(myFavouriteFood);*/
 let pokemonList = [
         {name : 'bulbasaur', height : 7, types : ['grass ', 'poison']},
         {name : 'squirtl', height: 5, types : "water"},
-        {name :' wartortle', height: 10, type : "water" },
+        {name : 'wartortle', height: 10, type : "water" },
 
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+      /* document.write(pokemonList[i]); this was my original code. obviously wrong*/
+        document.write("<p>" + pokemonList[i].name + "</p>");
+        // printing pokemonList[i]’s other details
+        console.log(pokemonList[i].name);
+        console.log(pokemonList[i].height);
+}
