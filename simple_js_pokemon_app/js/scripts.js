dc6107c86d7005b330deca00a51f8c577fade015
Peppermint-Mocha <jackimuthumbiyomade@gmail.com>
2021-11-02 15:42:33 +0100

    corrected for loop

--- simple_js_pokemon_app/js/scripts.js
+++ simple_js_pokemon_app/js/scripts.js
@@ -7,16 +7,17 @@ let myFavouriteFood="Falafel";
 document.write(myFavouriteFood);*/
 
 let pokemonList = [
-        {name : 'bulbasaur', height : 7, types : ['grass ', 'poison']},
-        {name : 'squirtl', height: 5, types : "water"},
-        {name : 'wartortle', height: 10, type : "water" },
+        {name : 'bulbasaur', height : 0.7, types : ['grass ', 'poison']},
+        {name : 'squirtl', height: 0.5, types : "water"},
+        {name : 'wartortle', height: 1.0, type : "water" },
 
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-      /* document.write(pokemonList[i]); this was my original code. obviously wrong*/
-        document.write("<p>" + pokemonList[i].name + "</p>");
-        // printing pokemonList[i]’s other details
-        console.log(pokemonList[i].name);
-        console.log(pokemonList[i].height);
+       if (pokemonList[i].height === 1){
+               document.write('<p>' + pokemonList[i].name + " is one huge pokemon!" + '</p>');
+       } else {
+        document.write('<p>' + pokemonList[i].name + '</p>');
+        document.write('<p>' + pokemonList[i].height + '</p>');
+       }
 }
