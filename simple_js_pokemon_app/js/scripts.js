ad302eb61c207464619bf82f8934c06409188142
Peppermint-Mocha <jackimuthumbiyomade@gmail.com>
2021-11-25 15:14:55 +0100

    Replaced document.write with functions and classes

--- simple_js_pokemon_app/js/scripts.js
+++ simple_js_pokemon_app/js/scripts.js
@@ -1,26 +1,12 @@
-//let pokemonRepository = (function () {
-  //let pokemonList = [{ name: 'weanut'}]; // empty array
-
-  //return {
-    //add: function(pokemon) {
-      //pokemonList.push(pokemon);
-    //},
-    //getAll: function() {
-    //  return pokemonList;
-    //}
-  //};
-//})();
-
-//console.log(pokemonRepository.getAll());
-//pokemonRepository.add({ type: 'poopsa'});
-//console.log(pokemonRepository.getAll());
 
 let pokemonRepository = (function() {
         let pokemonList = [
-                {name : 'bulbasaur', height : 0.7, type : ['grass', 'poison']},
-                {name : 'squirtl', height: 0.5, type : "water"},
-                {name : 'wartortle', height: 1.0, type : "water" },
-                ];
+                {name : 'Bulbasaur', height : 0.7, type : ['grass', 'poison']},
+                {name : 'Squirtl', height: 0.5, type : "water"},
+                {name : 'Wartortle', height: 1.0, type : "water" },
+                {name : 'Venusaur', height: 2,  type : ['grass,', 'poison']},
+                {name : 'Butterfree', height: 1.1, type : ['bug', 'flying']},
+        ];
     // adds new data to list
         function add(newPokemon) {
                 pokemonList.push(newPokemon);
@@ -29,23 +15,31 @@ let pokemonRepository = (function() {
         function getAll() {
                 return pokemonList;
         };
-    
+
     return {
             add: add,
             getAll: getAll
     };
 })();
 
-//console.log(pokemonRepository.getAll());
-//pokemonRepository.add({ name:'woopsa'});
-//console.log(pokemonRepository.getAll());
-
 // forEach loop that gets data from the pokemon Array and displays it
 pokemonRepository.getAll().forEach(function(newPokemon) {
         let pokemonName = newPokemon.name
         let pokemonHeight = newPokemon.height
         let pokemonType = newPokemon.type
-        // write to document
-        document.write(pokemonName + "  - Height: " + pokemonHeight + " - Powers:  " + pokemonType + '</p>');
+
+        //creates list and buttons within container div 
+        let main = document.querySelector('main');
+        main.classList.add('container');
+        let listPokemon = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li')
+        listItem.classList.add('list-unstyled'); //styling
+        let buttonOne = document.createElement('button');
+                buttonOne.innerText = pokemonName;
+                 buttonOne.classList.add('selected'); //styling
+
+                 //appends button to list and list to unordered list
+        listItem.appendChild(buttonOne);
+        listPokemon.appendChild(listItem);
         }
-        );
+ ); 
