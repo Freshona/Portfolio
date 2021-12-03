ee5feba71a730bbd9139c18f16455b46536c3263
Peppermint-Mocha <jackimuthumbiyomade@gmail.com>
2021-12-03 11:51:05 +0100

    Added query selectors and event handlers

--- simple_js_pokemon_app/js/scripts.js
+++ simple_js_pokemon_app/js/scripts.js
@@ -1,11 +1,11 @@
 
 let pokemonRepository = (function() {
         let pokemonList = [
+                {name : 'Butterfree', height: 1.1, type : ['bug', 'flying']},
                 {name : 'Bulbasaur', height : 0.7, type : ['grass', 'poison']},
-                {name : 'Squirtl', height: 0.5, type : "water"},
                 {name : 'Wartortle', height: 1.0, type : "water" },
                 {name : 'Venusaur', height: 2,  type : ['grass,', 'poison']},
-                {name : 'Butterfree', height: 1.1, type : ['bug', 'flying']},
+                {name : 'Squirtl', height: 0.5, type : "water" },
         ];
     // adds new data to list
         function add(newPokemon) {
@@ -16,30 +16,38 @@ let pokemonRepository = (function() {
                 return pokemonList;
         };
 
-    return {
-            add: add,
-            getAll: getAll
-    };
-})();
+        function addListItem(pokemon) {
+                let listPokemon = document.querySelector('.pokemon-list');
+                let listItem = document.createElement('li');
+                listItem.classList.add('list-unstyled');
+                //Button
+                let buttonOne = document.createElement('button');
+                  buttonOne.innerText = pokemon.name;
+                  buttonOne.classList.add('button-style');
+                listItem.appendChild(buttonOne);/*fixes button to list*/ 
+                listPokemon.appendChild(listItem);
+                //Button Event
+                buttonOne.addEventListener('click', function () { 
+                        showDetails(pokemon);
+                });
+              };
+          
 
-// forEach loop that gets data from the pokemon Array and displays it
-pokemonRepository.getAll().forEach(function(newPokemon) {
-        let pokemonName = newPokemon.name
-        let pokemonHeight = newPokemon.height
-        let pokemonType = newPokemon.type
+              function showDetails (pokemon) {
+                      console.log(pokemon);
+              }
 
-        //creates list and buttons within container div 
-        let main = document.querySelector('main');
-        main.classList.add('container');
-        let listPokemon = document.querySelector('.pokemon-list');
-        let listItem = document.createElement('li')
-        listItem.classList.add('list-unstyled'); //styling
-        let buttonOne = document.createElement('button');
-                buttonOne.innerText = pokemonName;
-                 buttonOne.classList.add('selected'); //styling
+              return {
+                      add: add,
+                      getAll: getAll,
+                      addListItem: addListItem
+              };   
+          })();
+          
+          pokemonRepository.getAll().forEach(function (pokemon){
+          pokemonRepository.addListItem(pokemon);
+          });
 
-                 //appends button to list and list to unordered list
-        listItem.appendChild(buttonOne);
-        listPokemon.appendChild(listItem);
-        }
- ); 
+
+     
+     
